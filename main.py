beab23c9bdc996eb1f5f47c5fec0023d2f02daa9
Petra Mikova <petramikova10@gmail.com>
2023-12-02 01:20:14 +0100

    functional version no refactoring

--- main.py
+++ main.py
@@ -1,130 +1,187 @@
 import random
-from collections import Counter
 import math
 import matplotlib.pyplot as plt
+import numpy as np
+from sklearn.neighbors import KDTree
 
+classified_data = []
+training_data = []
+red = []
+green = []
+blue = []
+purple = []
 
 class Point:
     def __init__(self, x, y, color):
         self.x = x
         self.y = y
         self.color = color
+        self.distance = -1
+
+def euclidean_distance(point1, point2):
+    return math.sqrt((point2.x - point1.x)**2 + (point2.y - point1.y)**2)
+
+def classify(x, y, k):
+    global training_data
+    global classified_data
+
+    for point in training_data:
+        point.distance = euclidean_distance(Point(x, y, ""), point)
+
+    sorted_training_data = sorted(training_data, key=lambda point: point.distance)
+
+    color_counts = {'red': 0, 'green': 0, 'blue': 0, 'purple': 0}
+    for point in sorted_training_data[:k]:
+        color_counts[point.color] += 1
+
+    new_color = max(color_counts, key=color_counts.get)
+    classified_data.append(Point(x, y, new_color))
+    training_data.append(Point(x, y, new_color))
+
+    return new_color
+
+def classify_kdtree(x, y, k):
+    global training_data
+    global classified_data
+
+    # Convert training_data to a numpy array for KD-tree
+    training_array = np.array([(point.x, point.y) for point in training_data])
+
+    # Create a KD-tree using the training data
+    tree = KDTree(training_array)
+
+    # Query the KD-tree to find the k nearest neighbors
+    _, indices = tree.query([(x, y)], k=k)
+    neighbors = [training_data[i] for i in indices[0]]
+
+    # Count the occurrences of each color in the neighbors
+    color_counts = {'red': 0, 'green': 0, 'blue': 0, 'purple': 0}
+    for neighbor in neighbors:
+        color_counts[neighbor.color] += 1
+
+    # Determine the most common color
+    new_color = max(color_counts, key=color_counts.get)
+    classified_data.append(Point(x, y, new_color))
+    training_data.append(Point(x, y, new_color))
+
+    return new_color
 
 
-def calculate_distance(point1, point2):
-    return math.sqrt((point1.x - point2.x) ** 2 + (point1.y - point2.y) ** 2)
-
-
-def classify(x, y, k, training_data):
-    new_point = Point(x, y, "")
-
-    distances = [(calculate_distance(new_point, point), point.color) for point in training_data]
-
-    k_nearest_neighbors = sorted(distances)[:k]
-
-    class_counts = Counter(neigh[1] for neigh in k_nearest_neighbors)
-
-    most_common_class = class_counts.most_common(1)[0][0]
-
-    return most_common_class
-
-
-def generate_starting_points(color, coordinates):
-    return [Point(x, y, color) for x, y in coordinates]
-
-
-# Define starting points for each class
 starting_points = {
-    'R': [(-4500, -4400), (-4100, -3000), (-1800, -2400), (-2500, -3400), (-2000, -1400)],
-    'G': [(4500, -4400), (4100, -3000), (1800, -2400), (2500, -3400), (2000, -1400)],
-    'B': [(-4500, 4400), (-4100, 3000), (-1800, 2400), (-2500, 3400), (-2000, 1400)],
-    'P': [(4500, 4400), (4100, 3000), (1800, 2400), (2500, 3400), (2000, 1400)]
+    'red': [(-4500, -4400), (-4100, -3000), (-1800, -2400), (-2500, -3400), (-2000, -1400)],
+    'green': [(4500, -4400), (4100, -3000), (1800, -2400), (2500, -3400), (2000, -1400)],
+    'blue': [(-4500, 4400), (-4100, 3000), (-1800, 2400), (-2500, 3400), (-2000, 1400)],
+    'purple': [(4500, 4400), (4100, 3000), (1800, 2400), (2500, 3400), (2000, 1400)]
 }
 
-# Generate starting points for each class
-starting_points = {class_: generate_starting_points(class_, coordinates) for class_, coordinates in
-                   starting_points.items()}
+def generate_points(number_of_points_per_color):
+    colors = ['red', 'green', 'blue', 'purple']
+    global red
+    global green
+    global blue
+    global purple
 
-
-def create_new_points_random(number_of_points_per_color):
-    points = []
-    colors = ['R', 'G', 'B', 'P']
-
-    for _ in range(number_of_points_per_color):
-        for color in colors:
-            unique_point = None
-
-            while unique_point is None:
-                if color == 'R':
+    for color in colors:
+        for _ in range(number_of_points_per_color):
+            if color == 'red':
+                if random.randint(1,100) > 1:
                     x = random.randint(-5000, 500)
                     y = random.randint(-5000, 500)
-                    if random.random() < 0.01:
-                        while x < 500 and y < 500:
-                            x = random.randint(-5000, 5000)
-                            y = random.randint(-5000, 5000)
-                elif color == 'G':
-                    x = random.randint(-500, 5000)
-                    y = random.randint(-5000, 500)
-                    if random.random() < 0.01:
-                        while x > -500 and y < 500:
-                            x = random.randint(-5000, 5000)
-                            y = random.randint(-5000, 5000)
-                elif color == 'B':
-                    x = random.randint(-5000, 500)
-                    y = random.randint(-500, 5000)
-                    if random.random() < 0.01:
-                        while x < 500 and y > -500:
-                            x = random.randint(-5000, 5000)
-                            y = random.randint(-5000, 5000)
                 else:
-                    x = random.randint(-500, 5000)
-                    y = random.randint(-500, 5000)
-                    if random.random() < 0.01:
-                        while x > -500 and y > -500:
-                            x = random.randint(-5000, 5000)
-                            y = random.randint(-5000, 5000)
+                    while x < 500 and y < 500:
+                        x = random.randint(-5000, 5000)
+                        y = random.randint(-5000, 5000)
 
                 point = Point(x, y, color)
-
-                # Check if the point is unique before adding it
-                if point not in points:
+                if point not in red:
                     unique_point = point
+                red.append(unique_point)
+            elif color == 'green':
+                if random.randint(1,100) > 1:
+                    x = random.randint(-500, 5000)
+                    y = random.randint(-5000, 500)
+                else:
+                    while x > -500 and y < 500:
+                        x = random.randint(-5000, 5000)
+                        y = random.randint(-5000, 5000)
 
-            points.append(unique_point)
+                point = Point(x, y, color)
+                if point not in green:
+                    unique_point = point
+                green.append(unique_point)
+            elif color == 'blue':
+                if random.randint(1,100) > 1:
+                    x = random.randint(-5000, 500)
+                    y = random.randint(-500, 5000)
+                else:
+                    while x < 500 and y > -500:
+                        x = random.randint(-5000, 5000)
+                        y = random.randint(-5000, 5000)
 
-    return points
+                point = Point(x, y, color)
+                if point not in blue:
+                    unique_point = point
+                blue.append(unique_point)
+            else:
+                if random.randint(1,100) > 1:
+                    x = random.randint(-500, 5000)
+                    y = random.randint(-500, 5000)
+                else:
+                    while x > -500 and y > -500:
+                        x = random.randint(-5000, 5000)
+                        y = random.randint(-5000, 5000)
+
+                point = Point(x, y, color)
+                if point not in purple:
+                    unique_point = point
+                purple.append(unique_point)
+
+NUMBER = 100
+generate_points(NUMBER)
 
 
-# Set parameters
-NUMBER_PER_COLOR = 1000
+def classify_point(choice, color, color_count, color_list, color_index):
+    global errors
 
-# Generate points using the specified distribution
-new_points = create_new_points_random(NUMBER_PER_COLOR)
+    if color_count < len(color_list[color_index]):
+        point = color_list[color_index][color_count]
+        x, y = point.x, point.y
+        color_count += 1
 
-# Plot the generated points
-plt.figure(figsize=(10, 10))
+        if choice == "kdtree":
+            new_color = classify_kdtree(x, y, k)
+        else:
+            new_color = classify(x, y, k)
 
-# Mapping of class names to colors
-color_mapping = {'R': 'red', 'G': 'green', 'B': 'blue', 'P': 'purple'}
+        if new_color != color:
+            errors += 1
+
+    return color_count
 
 
-training_data = starting_points['R'] + starting_points['G'] + starting_points['B'] + starting_points['P'] + new_points
+k_values = [1, 3, 7, 15]
 
-k_value = 3  # Replace with the desired k value
+if __name__ == "__main__":
+    for k in k_values:
+        choice = "kdtree"
+        errors = 0
+        training_data = [Point(x, y, color) for color, coordinates in starting_points.items() for x, y in coordinates]
+        classified_data = [Point(x, y, color) for color, coordinates in starting_points.items() for x, y in coordinates]
 
-# Apply classify function to each point in the generated list
-classified_points = [(point.x, point.y, classify(point.x, point.y, k_value, training_data)) for point in training_data]
+        color_counts = {'red': 0, 'green': 0, 'blue': 0, 'purple': 0}
+        color_list = {'red': red, 'green': green, 'blue': blue, 'purple': purple}
 
-# Print the classified points
-for x, y, color in classified_points:
-    print(f"Point at ({x}, {y}) is classified as: {color}")
+        while not all(count == NUMBER for count in color_counts.values()):
+            color = random.choice(['red', 'green', 'blue', 'purple'])
+            color_counts[color] = classify_point(choice, color, color_counts[color], color_list, color)
 
-# Plot the classified points
-plt.figure(figsize=(10, 10))
-for x, y, color in classified_points:
-    plt.scatter(x, y, c=color_mapping[color], marker='.', s=500)
+        print("Errors for k =", k, ":", errors)
 
-plt.title('Classified Points')
-plt.xlabel('X')
-plt.ylabel('Y')
-plt.show()
+        plt.figure(figsize=(10, 10))
+        for point in classified_data:
+            plt.scatter(point.x, point.y, c=point.color, marker='o', s=500)
+
+        plt.xlabel('X')
+        plt.ylabel('Y')
+        plt.show()
+
